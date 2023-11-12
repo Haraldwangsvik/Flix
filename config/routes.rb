0ea3dc6761bd3f8b1cc28803fc646e98d4a1eba3
Harald Wangsvik Fredriksen <98453497+Haraldwangsvik@users.noreply.github.com>
2023-11-12 13:21:48 +0100

    Ready to deploy v1.0

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  resources :genres
+  resources :favorites
+  resources :users
+  resource :session, only: [:new, :create, :destroy]
+  root "movies#index"
+
+  get "signup" => "users#new"
+  get "signin" => "sessions#new"
+  
+  get "movies/filter/:filter" => "movies#index", as: :filtered_movies
+
+  resources :movies do
+    resources :reviews
+    resources :favorites, only: [:create, :destroy]
+  end
+end
