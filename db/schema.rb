597d792606747061591503e9a0e9a3eb02b46044
Harald <haraldwf@stud.ntnu.no>
2023-11-15 17:55:10 +0100

    add gitattributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_11_12_130628) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_130628) do
   end
 
   create_table "characterizations", force: :cascade do |t|
-    t.integer "movie_id", null: false
-    t.integer "genre_id", null: false
+    t.bigint "movie_id", null: false
+    t.bigint "genre_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["genre_id"], name: "index_characterizations_on_genre_id"
@@ -49,8 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_130628) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "movie_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "movie_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_favorites_on_movie_id"
@@ -79,7 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_130628) do
   create_table "reviews", force: :cascade do |t|
     t.integer "stars"
     t.text "comment"
-    t.integer "movie_id", null: false
+    t.bigint "movie_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
